0a0a09e80340b48fcb91eaa872044cf2980b244b
“MIRABELLE-CHAFUMUKA” <bsc-com-25-22@unima.ac.mw>
2024-05-11 08:06:40 -0700

    creating layout of dashboard

--- src/componets/Dashboard/Dashboard.js
+++ src/componets/Dashboard/Dashboard.js
@@ -2,10 +2,66 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div className=''>
-        <h1 className=''>Dashboard</h1>
-      
+    <>
+    <div className="container mx-auto px-4 w-full h-60 shadow-md rounded-3xl bg-slate-400">
+      <div class="  grid grid-cols-2 gap-4 bg-slate-400 rounded-lg shadow-inner"> 
+      <div class="p-4">
+        <h3 class="text-3xl font-semibold">Best Offer</h3>
+        <div class="grid grid-cols-2 gap-2">
+          <div class="text-sm font-medium">HS HOSTEL</div>
+          <div class="text-sm font-medium">HS HOSTEL</div>
+          <div class="text-sm">LOCATION</div>
+          <div class="text-sm">LOCATION</div>
+        </div>
+        
     </div>
+    </div>
+    </div>
+  
+    <br></br>
+   
+    <div class="grid grid-cols-2 gap-4 place-items-center">
+    <div class=" container mx-sm px-4 w-85 h-60 shadow-md rounded-3xl bg-slate-400 p-4">
+      <div class=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
+      <div class="p-4">
+        <h3 class="text-lg font-semibold">Chikanda west</h3>
+        <div class ="conatiner rounded-2xl w-70 h-20 bg-blue-950 "> 
+        <button>Book Now</button>
+        </div>
+        </div>
+        </div>
+      
+        
+   </div>
+   <div class=" container mx-sm px-4 w-90 h-60 shadow-md rounded-3xl bg-slate-400 p-4">
+      <div class=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
+      <div class="p-4">
+        <h3 class="text-lg font-semibold">Chikanda west</h3>
+        <div class="grid grid-cols-2 gap-2"></div>
+        </div>
+        </div>
+        
+   </div>
+   <div class=" container mx-sm px-4 w-100 h-60 shadow-md rounded-3xl bg-slate-400 p-4">
+      <div class=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
+      <div class="p-4">
+        <h3 class="text-lg font-semibold">Chikanda west</h3>
+        <div class="grid grid-cols-2 gap-2"></div>
+        </div>
+        </div>
+        
+   </div>
+   <div class=" container mx-sm px-4 w-90 h-60 shadow-md rounded-3xl bg-slate-400 p-4">
+      <div class=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
+      <div class="p-4">
+        <h3 class="text-lg font-semibold">Chikanda west</h3>
+        <div class="grid grid-cols-2 gap-2"></div>
+        </div>
+        </div>
+        
+   </div>
+   </div>
+   </>
   )
 }
 
