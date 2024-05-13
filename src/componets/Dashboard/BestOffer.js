ef6bb4a61fe85e1278705e6ac01cd67c79c79191
“MIRABELLE-CHAFUMUKA” <bsc-com-25-22@unima.ac.mw>
2024-05-13 16:57:10 -0700

    changes made to dashboard. Now with pictured and details on bottom conatiners

---
+++ src/componets/Dashboard/BestOffer.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function BestOffer (props) {
+  return (
+    
+    <>    
+   
+    <div className= "grid items-end p-4">
+
+         <div className=" container mx-sm px-4 w-70 h-60 shadow-md rounded-3xl bg-slate-400 p-4 place-content-center">
+      <div className=" grid grid-cols-2 grid-row-2 gap-4 bg-slate-400 bg-blend-color  overflow-hidden"> 
+      <div className="p-4">
+      <img src={props.imgPath} alt='' ></img>
+        </div>
+      <div>
+        <div className="conatiner bg-slate-300 rounded-xl w-50">
+        <span className= "text-lg font-bold font-calibre text-center ">{props.name}</span>
+        <p className='rent font-bold text-center'>RENT PRICE</p>
+        <h1>{props.price}</h1>
+        <p>{props.location}</p>
+        <p>{props.place}</p>
+        </div>
+        <br></br>
+        <button className='button bg-slate-950 rounded text-white h-8 w-20 font-serif'>Book Now</button>
+        </div>
+        </div>     
+        </div>
+        </div>
+        
+        
+      </>
+      
+ )
+}
+
+export default BestOffer
--- src/componets/Dashboard/Dashboard.js
+++ src/componets/Dashboard/Dashboard.js
@@ -1,106 +1,26 @@
 import React from 'react'
+import BestOffer from './BestOffer'
+import BestOfferData from './BestOfferData'
 
 function Dashboard() {
   return (
     <>
     <div className="grid grid-cols-3 gap-2 p-8">
-<div> </div>
-
-    <div className="container mx-auto px-4 w-90 h-60 shadow-2xl rounded-3xl bg-slate-400 col-span-2">
-      
-        <h3 className="text-3xl font-semibold font-serif">Best Offer</h3>
-              
+<div></div>
+<div className="container mx-auto px-4 w-90 h-60 shadow-2xl rounded-3xl bg-slate-400 col-span-2">
+    <div className="p-4">
+    <h3 className="text-3xl font-semibold">Best Offer</h3>
     </div>
+        </div>
     </div>
-    
-    <br></br>
-    <div className="grid grid-cols-2 gap-4 place-items-start p-4">
-      <div>side bar</div>
+    <div className='grid grid-cols-2'>
+    {BestOfferData.map((item,index)=>{
+      return( 
+        <BestOffer imgPath ={item.imgPath} name={item.name} price={item.price} location={item.location}></BestOffer>
+      )
       
-    <div className="grid grid-cols-2 gap-4 place-items-center">
-    <div className=" container mx-sm px-4 w-90 h-60 shadow-md rounded-3xl bg-slate-400 p-4 ">
-      <div className=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
-      <div className="p-4">
-        <h3 className="text-lg font-semibold">Chikanda west</h3>
-        <div className="grid grid-cols-2 gap-2"></div>
-        </div>
-      <div>
-        <div className="conatiner bg-slate-300 rounded-xl w-50 ">
-        <h1 className="text-lg font-bold font-calibre text-center ">Hostel</h1>
-        <p className='rent font-bold text-center'>RENT PRICE</p>
-        <p className='price text-center'>K70,000</p>
-        <p className='rent font-bold text-center'>LOCATION</p>
-        <p className='text-center'>Matiya</p>
-        </div>
-        <br></br>
-        <button className='button bg-slate-950 rounded text-white h-8 w-20 font-serif'>Book Now</button>
-        </div>
-        </div>     
-        </div>
-
-      <div className=" container mx-sm px-4 w-90 h-60 shadow-md rounded-3xl bg-slate-400 p-4">
-      <div className=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
-      <div className="p-4">
-        <h3 className="text-lg font-semibold">Chikanda west</h3>
-        <div className="grid grid-cols-2 gap-2"></div>
-        </div>
-        <div>
-        <div className="conatiner bg-slate-300 rounded-xl w-50">
-        <h1 className="text-lg font-bold font-calibre text-center ">Hostel</h1>
-        <p className='rent font-bold text-center'>RENT PRICE</p>
-        <p className='price text-center'>K70,000</p>
-        <p className='rent font-bold text-center'>LOCATION</p>
-        <p className='text-center'>Matiya</p>
-        </div>
-        <br></br>
-        <button className='button bg-slate-950 rounded text-white h-8 w-20 font-serif'>Book Now</button>
-        </div>
-        </div>
-        
+    })}
    </div>
-   <div className=" container mx-sm px-4 w-100 h-60 shadow-md rounded-3xl bg-slate-400 p-4">
-      <div className=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
-      <div className="p-4">
-        <h3 className="text-lg font-semibold">Chikanda west</h3>
-        <div className="grid grid-cols-2 gap-2"></div>
-        </div>
-        <div>
-        <div className="conatiner bg-slate-300 rounded-xl w-50">
-        <h1 className="text-lg font-bold font-calibre text-center ">Hostel</h1>
-        <p className='rent font-bold text-center'>RENT PRICE</p>
-        <p className='price text-center'>K70,000</p>
-        <p className='rent font-bold text-center'>LOCATION</p>
-        <p className='text-center'>Matiya</p>
-        </div>
-        <br></br>
-        <button className='button bg-slate-950 rounded text-white h-8 w-20 font-serif'>Book Now</button>
-        </div>
-        </div>
-        
-   </div>
-   <div className=" container mx-sm px-4 w-90 h-60 shadow-md rounded-3xl bg-slate-400 p-4">
-      <div className=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
-      <div className="p-4">
-        <h3 className="text-lg font-semibold">Chikanda west</h3>
-        <div className="grid grid-cols-2 gap-2"></div>
-        </div>
-        <div>
-        <div className="conatiner bg-slate-300 rounded-xl w-50">
-        <h1 className="text-lg font-bold font-calibre text-center ">Hostel</h1>
-        <p className='rent font-bold text-center'>RENT PRICE</p>
-        <p className='price text-center'>K70,000</p>
-        <p className='rent font-bold text-center'>LOCATION</p>
-        <p className='text-center'>Matiya</p>
-        </div>
-        <br></br>
-        <button className='button bg-slate-950 rounded text-white h-8 w-20 font-serif'>Book Now</button>
-        </div>
-        </div>
-        
-   </div>
-   </div>
-   </div>
-  
    </>
   )
 }
