ca94f7cf75799d8421bc4684891dee99fd47bcb4
“Cassim <bed-com-24-22@unima.ac.mw>
2024-05-14 13:07:01 +0200

    Restoring the file

--- src/componets/Dashboard/Dashboard.js
+++ src/componets/Dashboard/Dashboard.js
@@ -2,58 +2,42 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div className='items-center p-20 z-0'>
-    <div className="grid grid-cols-3 gap-2 p-8">
-<div> </div>
-
-    <div className="container mx-auto px-4 w-90 h-60 shadow-2xl rounded-3xl bg-slate-400 col-span-2">
-      
-        <h3 className="text-3xl font-semibold font-serif">Best Offer</h3>
-              
+    <div className='items-center p-20 '>
+    <div className="container mx-auto px-4 w-full h-60 shadow-md rounded-3xl bg-slate-400">
+      <div className="  grid grid-cols-2 gap-4 bg-slate-400 rounded-lg shadow-inner"> 
+      <div className="p-4">
+        <h3 className="text-3xl font-semibold">Best Offer</h3>
+        <div className="grid grid-cols-2 gap-2">
+          <div className="text-sm font-medium">HS HOSTEL</div>
+          <div className="text-sm font-medium">HS HOSTEL</div>
+          <div className="text-sm">LOCATION</div>
+          <div className="text-sm">LOCATION</div>
+        </div>
+        
     </div>
     </div>
-    
+    </div>
+  
     <br></br>
-    <div className="grid grid-cols-2 gap-4 place-items-start p-4">
-      <div>side bar</div>
-      
+   
     <div className="grid grid-cols-2 gap-4 place-items-center">
-    <div className=" container mx-sm px-4 w-90 h-60 shadow-md rounded-3xl bg-slate-400 p-4 ">
+    <div className=" container mx-sm px-4 w-85 h-60 shadow-md rounded-3xl bg-slate-400 p-4">
+      <div className=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
+      <div className="p-4">
+        <h3 className="text-lg font-semibold">Chikanda west</h3>
+        <div className ="conatiner rounded-2xl w-70 h-20 bg-blue-950 "> 
+        <button>Book Now</button>
+        </div>
+        </div>
+        </div>
+      
+        
+   </div>
+   <div className=" container mx-sm px-4 w-90 h-60 shadow-md rounded-3xl bg-slate-400 p-4">
       <div className=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
       <div className="p-4">
         <h3 className="text-lg font-semibold">Chikanda west</h3>
         <div className="grid grid-cols-2 gap-2"></div>
-        </div>
-      <div>
-        <div className="conatiner bg-slate-300 rounded-xl w-50 ">
-        <h1 className="text-lg font-bold font-calibre text-center ">Hostel</h1>
-        <p className='rent font-bold text-center'>RENT PRICE</p>
-        <p className='price text-center'>K70,000</p>
-        <p className='rent font-bold text-center'>LOCATION</p>
-        <p className='text-center'>Matiya</p>
-        </div>
-        <br></br>
-        <button className='button bg-slate-950 rounded text-white h-8 w-20 font-serif'>Book Now</button>
-        </div>
-        </div>     
-        </div>
-
-      <div className=" container mx-sm px-4 w-90 h-60 shadow-md rounded-3xl bg-slate-400 p-4">
-      <div className=" grid grid-cols-2 gap-4 bg-slate-400 bg-blend-color box-shadow-md overflow-hidden"> 
-      <div className="p-4">
-        <h3 className="text-lg font-semibold">Chikanda west</h3>
-        <div className="grid grid-cols-2 gap-2"></div>
-        </div>
-        <div>
-        <div className="conatiner bg-slate-300 rounded-xl w-50">
-        <h1 className="text-lg font-bold font-calibre text-center ">Hostel</h1>
-        <p className='rent font-bold text-center'>RENT PRICE</p>
-        <p className='price text-center'>K70,000</p>
-        <p className='rent font-bold text-center'>LOCATION</p>
-        <p className='text-center'>Matiya</p>
-        </div>
-        <br></br>
-        <button className='button bg-slate-950 rounded text-white h-8 w-20 font-serif'>Book Now</button>
         </div>
         </div>
         
@@ -64,17 +48,6 @@ function Dashboard() {
         <h3 className="text-lg font-semibold">Chikanda west</h3>
         <div className="grid grid-cols-2 gap-2"></div>
         </div>
-        <div>
-        <div className="conatiner bg-slate-300 rounded-xl w-50">
-        <h1 className="text-lg font-bold font-calibre text-center ">Hostel</h1>
-        <p className='rent font-bold text-center'>RENT PRICE</p>
-        <p className='price text-center'>K70,000</p>
-        <p className='rent font-bold text-center'>LOCATION</p>
-        <p className='text-center'>Matiya</p>
-        </div>
-        <br></br>
-        <button className='button bg-slate-950 rounded text-white h-8 w-20 font-serif'>Book Now</button>
-        </div>
         </div>
         
    </div>
@@ -84,23 +57,10 @@ function Dashboard() {
         <h3 className="text-lg font-semibold">Chikanda west</h3>
         <div className="grid grid-cols-2 gap-2"></div>
         </div>
-        <div>
-        <div className="conatiner bg-slate-300 rounded-xl w-50">
-        <h1 className="text-lg font-bold font-calibre text-center ">Hostel</h1>
-        <p className='rent font-bold text-center'>RENT PRICE</p>
-        <p className='price text-center'>K70,000</p>
-        <p className='rent font-bold text-center'>LOCATION</p>
-        <p className='text-center'>Matiya</p>
-        </div>
-        <br></br>
-        <button className='button bg-slate-950 rounded text-white h-8 w-20 font-serif'>Book Now</button>
-        </div>
         </div>
         
    </div>
    </div>
-   </div>
-  
    </div>
   )
 }
