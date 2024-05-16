5817b018a89040e628f3d050ea7cbf61b3cc3938
EDWARDYOUNGSHABA <167221665+EDWARDYOUNGSHABA@users.noreply.github.com>
2024-05-16 01:42:05 +0100

    edward shaba adding style in booking component

--- src/componets/Booking/HouseList.js
+++ src/componets/Booking/HouseList.js
@@ -2,15 +2,17 @@ import { Link } from 'react-router-dom';
 
 function ImageLink({ imgPath, name, price, roomCapacity, status }) {
   return (
-    <div className='relative h-28 bg-slate-300 pt-2 flex items-center m-auto pl-10 pr-10'>
+    <div className='relative h-28 bg-slate-300 pt-2 flex items-center m-auto pl-10 pr-10 border-4 border-gray-400 rounded-lg p-4'>
       <Link to='/Details' className='flex-shrink-0'>
         <img src={imgPath} alt='' className='w-32 h-24 cursor-pointer' />
       </Link>
       <div className='flex-grow flex justify-between items-center pl-10'>
-        <span>{name}</span>
-        <h1>{price}</h1>
-        <h1>{roomCapacity}</h1>
-        <h1 className='pr-10'>{status}</h1>
+        <span className="bg-slate-300 p-6 rounded-full shadow-md">{name}</span>
+        <h1 className="bg-slate-300 p-6 rounded-full shadow-md">{price}</h1>
+        <h1 className="bg-slate-300 p-6 rounded-full shadow-md">{roomCapacity}</h1>
+        <h1 className='pr-10 bg-slate-300 p-6 rounded-full shadow-md'>{status}</h1>
+        
+        <button className='button bg-blue-600 rounded-full text-white h-8 w-20 font-serif'>Book Now</button>
       </div>
     </div>
   );
--- src/componets/Booking/HouseListData.js
+++ src/componets/Booking/HouseListData.js
@@ -12,7 +12,92 @@ const HouseListData = [
         price: "K89,000.00",
         status:"booked",
         roomCapacity:"Single"
+    },
+    {
+        imgPath: "assets/houses/house3.jpg",
+        hostelName: "Matiya",
+        price: "K56,000.00",
+        status:"Available",
+        roomCapacity:"double"
+    },
+    {
+        imgPath: "assets/houses/house4.jpg",
+        hostelName: "Ndakhalira",
+        price: "70,000.00",
+        status:"Available",
+        roomCapacity:"3 per bed"
+    },
+    {
+        imgPath: "assets/houses/house5.jpg",
+        hostelName: "Edward",
+        price: "K56,000.00",
+        status:"Available",
+        roomCapacity:"double"
+    },
+    {
+        imgPath: "assets/houses/house6.jpg",
+        hostelName: "Chikanda SDA",
+        price: "K50,000.00",
+        status:"Available",
+        roomCapacity:"Single"
+    },
+    {
+        imgPath: "assets/houses/house7.jpg",
+        hostelName: "Makawa",
+        price: "K65,000.00",
+        status:"Available",
+        roomCapacity:"double"
+    },
+    {
+        imgPath: "assets/houses/house8.jpg",
+        hostelName: "Zuze Hostel",
+        price: "K73,000.00",
+        status:"Available",
+        roomCapacity:"double"
+    },
+    {
+        imgPath: "assets/houses/house9.jpg",
+        hostelName: " Hostel",
+        price: "K60,000.00",
+        status:"booked",
+        roomCapacity:"Single"
+    },
+    {
+        imgPath: "assets/houses/house10.jpg",
+        hostelName: "Petite",
+        price: "K56,000.00",
+        status:"Available",
+        roomCapacity:"Single"
+    },
+    {
+        imgPath: "assets/houses/house11.jpg",
+        hostelName: "Indiana",
+        price: "K70,000.00",
+        status:"booked",
+        roomCapacity:"double"
+    },
+    {
+        imgPath: "assets/houses/house12.jpg",
+        hostelName: "Chikowi",
+        price: "K80,000.00",
+        status:"Available",
+        roomCapacity:"double"
+    },
+    {
+        imgPath: "assets/houses/house13.jpg",
+        hostelName: "Mpesa",
+        price: "K57,000.00",
+        status:"Available",
+        roomCapacity:"double"
+    },
+    {
+        imgPath: "assets/houses/house14.jpg",
+        hostelName: "Zapani",
+        price: "74,000.00",
+        status:"Available",
+        roomCapacity:"Single"
     }
+    
 ]
 
 export default HouseListData
