c9317de2291c1cfa9ee8a4023507a71f7ec2df2c
sestinoSimbani <167290225+sestinoSimbani@users.noreply.github.com>
2024-05-21 19:22:17 -0700

    adding styles

--- src/componets/About/AboutList.js
+++ src/componets/About/AboutList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function AboutList(props) {
   return (
-    <div className='flex flex-col items-center bg: transparent p-4 rounded-lg m-auto '>
+    <div className='flex flex-col items-center bg-transparent p-4 rounded-lg m-auto -mx-1'>
       <img src={props.imgpath} alt='Imagey' className='w-16 h-16 rounded-full mb-4 cursor-pointer' /> 
       <div className='flex flex-col items-center'>
         <span className="text-lg font-semibold text-gray-800 mb-2">{props.name}</span>
