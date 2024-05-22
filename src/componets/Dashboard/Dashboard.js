182775be2b8d20c31d31f18115c9d6e72525e7de
MIRABELLE-CHAFUMUKA <bsc-com-25-22@unima.ac.mw>
2024-05-22 17:33:38 +0200

    Adding brief description of Project to dashboard

--- src/componets/Dashboard/Dashboard.js
+++ src/componets/Dashboard/Dashboard.js
@@ -8,10 +8,10 @@ function Dashboard() {
       <div className=" w-10/12 p-10">
         <div style= {{backgroundImage: `url(${imagePath})`}}className="mx-auto px-4 w-full h-96 shadow-2xl rounded-3xl  flex flex-col justify-start items-start p-10 bg-cover">
           <div className="text-center place-content-start w-1/2 align-middle ">
-          <div className="text-xl font-semibold font-serif text-white">We are here to give you a Fast,Simple and Secure way to</div>
-          <div className="text-xl font-semibold font-serif text-white">find accommodation during your studies.</div>
+          <div className="text-2xl font-semibold font-serif text-white">We are here to give you a Fast,Simple and Secure way to</div>
+          <div className="text-2xl font-semibold font-serif text-white">find accommodation during your studies.</div>
           <div></div>
-              <div className="text-xl font-serif font-bold text-white">Secure your spot today!</div>
+              <div className="text-2xl font-serif font-bold text-white">Secure your spot today!</div>
           </div>
         </div>
       </div>
