4b91f3c218e305552a42ad8ba8c287e85bc9a5c1
“Cassim <bed-com-24-22@unima.ac.mw>
2024-05-22 05:47:47 +0200

    centered the Nopay icon

--- src/componets/Payments/Payments.js
+++ src/componets/Payments/Payments.js
@@ -4,8 +4,8 @@ import React from 'react'
 function Payments() {
   return (
     <div className="container items-center m-auto">
-        <img src='assets/icons/nopay.png' alt='NoPay' className=' relative w-52 h-52 m-auto top-52' ></img>   
-        <h1 className=' relative top-52'>No payement made</h1>
+        <img src='assets/icons/nopay.png' alt='NoPay' className=' relative w-52 h-52 m-auto top-40' ></img>   
+        <h1 className=' relative top-40'>No payement made</h1>
     </div>
   )
 }
